64d726eae1758ba78cdd508d227572b40d2ba989
vimal <vimal@gmail.com>
2021-08-06 06:51:59 +0000

    First Commit

---
+++ MyTestApp/MyTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTestApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
